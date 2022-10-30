a5d87cec3fe9dc451d2ee64dcdabacd8a72b18fe
Mandofskii <59086060+Mandofskii@users.noreply.github.com>
2022-10-30 21:12:18 +0330

    I don't know what I did

--- src/counter.go
+++ src/counter.go
@@ -2,78 +2,59 @@ package src
 
 import (
 	"bufio"
-	"io/ioutil"
 	"log"
 	"os"
 	"unicode/utf8"
+	"fmt"
+	"strings"
 )
-func CharacterAndLineCounter(cmd string) (int,int){
-	var counts int
+func CharacterAndLineCounter(cmd string){
+	counts := 0
 	lines := 0
+	isPrinted := false
 	file, err := os.Open(cmd)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		text := scanner.Text()
-		lines ++
-		counts += utf8.RuneCountInString(text)
-	}
-	if err := scanner.Err(); err != nil {
+	
+	fileInfo, err := file.Stat()
+	if err != nil {
 		log.Fatal(err)
 	}
-	return counts,lines
-}
 
-func DirectoryCharacterAndLineCounter(cmd string) (int,int){
-	var counts int
-	lines := 0
-	files, err := ioutil.ReadDir(cmd)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    for _, f := range files {
-		if f.IsDir(){
-			files2, err := ioutil.ReadDir(cmd+f.Name()+"/")
-			if err != nil {
-				log.Fatal(err)
-			}
-			for _, f2 := range files2 {
-				file,err := os.Open(cmd+f.Name()+"/"+f2.Name())
-				if err != nil{
-					log.Fatal(err)
-				}
-				defer file.Close()
-				scanner := bufio.NewScanner(file)
-				for scanner.Scan(){
-					text := scanner.Text()
-					lines ++
-					counts += utf8.RuneCountInString(text)
-				}
-				if err := scanner.Err(); err != nil{
-					log.Fatal(err)
-				}
-			}
-			continue
-		}
-		file,err := os.Open(cmd+f.Name())
-		if err != nil{
+	if fileInfo.IsDir() {
+		files, err := file.Readdir(0)
+		if err != nil {
 			log.Fatal(err)
 		}
-		defer file.Close()
+		if !strings.HasSuffix(cmd, "/") {
+			cmd += "/"
+		}
+		for _, v := range files {
+			if v.IsDir() {
+				fmt.Println("Directory: ", cmd + v.Name())
+				CharacterAndLineCounter(cmd + v.Name())	
+			} else {
+				CharacterAndLineCounter(cmd + v.Name())
+			}
+			
+		}		
+	} else {
+		fmt.Println("File: ", cmd)
+		isPrinted = true
 		scanner := bufio.NewScanner(file)
-		for scanner.Scan(){
+		for scanner.Scan() {
 			text := scanner.Text()
 			lines ++
 			counts += utf8.RuneCountInString(text)
 		}
-		if err := scanner.Err(); err != nil{
+		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
-    }
-	return counts,lines
+	}
+	if isPrinted {
+		fmt.Println("Lines: ", lines)
+		fmt.Println("Characters: ", counts)
+	}
 }
